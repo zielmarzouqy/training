b0e84bb84fb9d1e42d75abdd9963337ae00e64ec
zielmarzouqy <zi.elmarzouqy@gmail.com>
2020-06-05 02:34:48 +0200

    add filter resources for configuration connection.

--- src/main/java/org/training/training/controller/MyController.java
+++ src/main/java/org/training/training/controller/MyController.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.training.training.dao.Connection;
 import org.training.training.service.ICommande;
 
 @Controller
@@ -16,6 +17,9 @@ public class MyController {
 
    @Autowired
    ICommande commandeImpl;
+   
+   @Autowired
+   Connection connection;
 //   
 //   @Autowired
 //   ICommande commandeImpl2;
@@ -24,6 +28,7 @@ public class MyController {
    public String index(Model model) {
       model.addAttribute("message", "Hello Spring MVC 5!");
       commandeImpl.showCommande();
+      System.out.println(connection);
       logger.debug("This is debug message");
       logger.info("This is info message");
       
